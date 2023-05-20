85f05308849c231a5778ef1c4b8160d782fc660b
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-05-20 23:22:54 +0330

    fix dependency error

--- pages/panel/articles/[slug]/index.tsx
+++ pages/panel/articles/[slug]/index.tsx
@@ -216,7 +216,7 @@ function EditArticles() {
           </button>
         </div>
 
-        {preview && <CreateArticlePreview title={title} content={content} />}
+        {preview && <CreateArticlePreview title={title} content={content} previewHandler={previewHandler} />}
 
         {uploadArticleImage && (
           <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
